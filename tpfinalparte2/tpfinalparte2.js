5b7fc541806a97c2821ef81306ff08cedf9fd922
juliamiccino <miccinojulia.11@gmail.com>
2025-11-16 09:45:59 -0300

    tpfinalparte2.js

--- tpfinalparte2/tpfinalparte2.js
+++ tpfinalparte2/tpfinalparte2.js
@@ -1,4 +1,5 @@
 // Comisión 1 Matías Arias y Julia Miccino
+// https://www.youtube.com/watch?v=UH_Bx80HZWA
 let caja;
 
 function preload(){
@@ -22,4 +23,5 @@ function draw() {
 function mousePressed(){
 caja.pantallas();
 }
+
 
